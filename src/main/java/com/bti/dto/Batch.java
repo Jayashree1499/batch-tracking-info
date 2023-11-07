2be237f6d7c1d95c9475f3cd1208630db8b21a3f
Jayashree1499 <jayasn14@gmail.com>
2023-11-07 12:37:23 +0530

    batch and image dto created

--- src/main/java/com/bti/dto/Batch.java
+++ src/main/java/com/bti/dto/Batch.java
@@ -1,5 +1,34 @@
 package com.bti.dto;
 
-public class Batch {
+import java.time.LocalDate;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Component
+public class Batch {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "batch_id")
+	@SequenceGenerator(name = "batch_id", initialValue = 201, allocationSize = 1)
+	private int id;
+	private String subject;
+	private LocalDate createDate;
+
+	private Status status;
+
+	@OneToMany
+	private List<Image> image;
 }
